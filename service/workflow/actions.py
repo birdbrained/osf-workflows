283a7dd4edbd8427ceb93db389f71ea91c108235
Joshua Bird <joshua.thomas.bird@gmail.com>
2017-05-10 10:37:19 -0400

    Update actions.py

--- service/workflow/actions.py
+++ service/workflow/actions.py
@@ -1,7 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+
+Action Behaviors
+================
+
+Implementations for the various behaviors action objects may have.
+
+
+"""
+
 from workflow import models
 
 
 def request_data(case, action):
+    """Requests a piece of data by creating a message
+    that is displayed by the submission form."""
     request_message = models.Message(
         message_type='Request',
         view=action.output.view,
